c88e87e9c9c9a062050808e40dfc6ec44de98f47
sothaker <44724341+sothaker@users.noreply.github.com>
2018-12-03 19:51:30 -0800

    Timer and Score Text

--- drone/src/Board.java
+++ drone/src/Board.java
@@ -1,178 +1,171 @@
 
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Toolkit;
-import java.awt.geom.Rectangle2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import javax.swing.Timer;
 
 public class Board extends JPanel implements MouseListener {
 
-    boolean ingame = true;
-    private int BOARD_WIDTH;
-    private int BOARD_HEIGHT;
-    private Set<Character> characters;
-    private Drone d;
+	boolean ingame = true;
+	private int BOARD_WIDTH;
+	private int BOARD_HEIGHT;
+	private Set<Character> characters;
+	private Drone d;
+	private long startTime;
 
-    public Board() {
-        addMouseListener();
-        
-        characters = new HashSet<>();
-        characters = Collections.synchronizedSet(characters);
-    }
+	public Board() {
+		addMouseListener();
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+		characters = new HashSet<>();
+		characters = Collections.synchronizedSet(characters);
+		startTime = System.currentTimeMillis();
+	}
 
-        Character c;
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
 
-        synchronized (characters) {
-            Iterator<Character> it = characters.iterator();
-            while (it.hasNext()) {
-                c = it.next();
+		Character c;
 
-                // Removes characters that are off screen
-                if (c.x < -650) {
-                    it.remove();
-                }
+		synchronized (characters) {
+			Iterator<Character> it = characters.iterator();
+			while (it.hasNext()) {
+				c = it.next();
 
-                if (c.isDrone()) {
-                    // Prevents drone from going out of bounds on the left
-                    if (c.x < -70) {
-                        c.moveLeft = false;
-                        c.setXVelocity(0);
-                    }
-                    
-                    // Prevents drone from going out of bounds on the right
-                    if (c.x > BOARD_WIDTH - 125){
-                        c.moveRight = false;
-                        c.setXVelocity(0);
-                    }
-                    
-                    // Prevents drone from going out of bounds on the top
-                    if (c.y < -80){
-                        c.moveUp = false;
-                        c.setYVelocity(0);
-                    }
-                    
-                    // Prevents drone from going out of bounds on the bottom
-                    if (c.y > BOARD_HEIGHT - 115){
-                        c.moveDown = false;
-                        c.setYVelocity(0);
-                    }
-                }
-                
-                // Applies gravity and friction to all characters
-                g = c.getGraphic(g);
-                c.y -= c.decelerateGravity();
-                c.x += c.decelerateFriction();
+				// Removes characters that are off screen
+				if (c.x < -650) {
+					it.remove();
+				}
 
-                if (c.moveUp) {
-                    c.y -= c.accelerateY();
-                    c.resetAcceleration();
-                }
+				if (c.isDrone()) {
+					// Prevents drone from going out of bounds on the left
+					if (c.x < -70) {
+						c.moveLeft = false;
+						c.setXVelocity(0);
+					}
 
-                if (c.moveDown) {
-                    c.y -= c.decelerateY();
-                    c.resetAcceleration();
-                }
+					// Prevents drone from going out of bounds on the right
+					if (c.x > BOARD_WIDTH - 125) {
+						c.moveRight = false;
+						c.setXVelocity(0);
+					}
 
-                if (c.moveRight) {
-                    c.x += c.accelerateX();
-                    c.resetAcceleration();
-                }
+					// Prevents drone from going out of bounds on the top
+					if (c.y < -80) {
+						c.moveUp = false;
+						c.setYVelocity(0);
+					}
 
-                if (c.moveLeft) {
-                    c.x += c.decelerateX();
-                    c.resetAcceleration();
-                }
-            }
-        }
+					// Prevents drone from going out of bounds on the bottom
+					if (c.y > BOARD_HEIGHT - 115) {
+						c.moveDown = false;
+						c.setYVelocity(0);
+					}
+				}
 
-        Font small = new Font("Helvetica", Font.BOLD, 14);
-        FontMetrics metr = this.getFontMetrics(small);
-        g.setColor(Color.black);
-        g.setFont(small);
-        //g.drawString(message, 10, d.height-60);
+				// Applies gravity and friction to all characters
+				g = c.getGraphic(g);
+				c.y -= c.decelerateGravity();
+				c.x += c.decelerateFriction();
 
-        if (ingame) {
+				if (c.moveUp) {
+					c.y -= c.accelerateY();
+					c.resetAcceleration();
+				}
 
-            // g.drawImage(img,0,0,200,200 ,null);
-        }
-        Toolkit.getDefaultToolkit().sync();
-        g.dispose();
-    }
-    
-    private void addMouseListener(){
-        addMouseListener(this);
-    }
-    
-    void addCharacter(Character c) {
-        if (c.isDrone()){
-            d = (Drone) c;
-        }
-        characters.add(c);
-    }
+				if (c.moveDown) {
+					c.y -= c.decelerateY();
+					c.resetAcceleration();
+				}
 
-    Set<Character> getCharacters() {
-        return characters;
-    }
-    
-    Drone getDrone(){
-        return d;
-    }
-    
-    void setDimension(Dimension d){
-        BOARD_WIDTH = d.width;
-        BOARD_HEIGHT = d.height;
-        setBackgroundPicture();
-    }
-    
-    private void setBackgroundPicture(){
-        JLabel background;
-        setSize(BOARD_WIDTH, BOARD_HEIGHT);
-        setLayout(null);
-        ImageIcon img = new ImageIcon("images/background.png");
-        background = new JLabel("",img,JLabel.CENTER);
-        background.setBounds(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
-        add(background);
-    }
+				if (c.moveRight) {
+					c.x += c.accelerateX();
+					c.resetAcceleration();
+				}
 
-    public void mousePressed(MouseEvent e) {
-        /*
-        int x = e.getX();
-        int y = e.getY();
-         */
+				if (c.moveLeft) {
+					c.x += c.decelerateX();
+					c.resetAcceleration();
+				}
+			}
+		}
 
-    }
+		Font small = new Font("Helvetica", Font.BOLD, 24);
+		g.setColor(Color.black);
+		g.setFont(small);
+		g.drawString((System.currentTimeMillis() - startTime) / 1000 - 2 + " seconds", 10, 20);
+		g.drawString("Collisions: " + CollisionMonitorThread.collisions, BOARD_WIDTH / 2, 20);
+		g.drawString("Games Won: 0/0", BOARD_WIDTH - 200, 20);
+		Toolkit.getDefaultToolkit().sync();
+		g.dispose();
+	}
 
-    public void mouseReleased(MouseEvent e) {
+	private void addMouseListener() {
+		addMouseListener(this);
+	}
 
-    }
+	void addCharacter(Character c) {
+		if (c.isDrone()) {
+			d = (Drone) c;
+		}
+		characters.add(c);
+	}
 
-    public void mouseEntered(MouseEvent e) {
+	Set<Character> getCharacters() {
+		return characters;
+	}
 
-    }
+	Drone getDrone() {
+		return d;
+	}
 
-    public void mouseExited(MouseEvent e) {
+	void setDimension(Dimension d) {
+		BOARD_WIDTH = d.width;
+		BOARD_HEIGHT = d.height;
+		setBackgroundPicture();
+	}
 
-    }
+	private void setBackgroundPicture() {
+		JLabel background;
+		setSize(BOARD_WIDTH, BOARD_HEIGHT);
+		setLayout(null);
+		ImageIcon img = new ImageIcon("images/background.png");
+		background = new JLabel("", img, JLabel.CENTER);
+		background.setBounds(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+		add(background);
+	}
 
-    public void mouseClicked(MouseEvent e) {
+	public void mousePressed(MouseEvent e) {
 
-    }
+	}
 
-}//end of class
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+}// end of class
