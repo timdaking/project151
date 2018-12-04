aee03bf1396618925779f403769d7329d48e0ae5
sothaker <44724341+sothaker@users.noreply.github.com>
2018-12-03 19:50:27 -0800

    Change to track collisions to keep score

--- drone/src/CollisionMonitorThread.java
+++ drone/src/CollisionMonitorThread.java
@@ -2,68 +2,61 @@
 import java.util.Iterator;
 import java.util.Set;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Unknown
  */
 class CollisionMonitorThread implements Runnable {
-    private final Thread t;
-    private final Board b;
-    private Set<Character> characters;
-    private final Drone d;
-    private volatile boolean shouldStop = false;
-    
-    CollisionMonitorThread(Board b){
-        t = new Thread(this, "Collision Monitor Thread");
-        this.b = b;
-        d = b.getDrone();
-    }
-    
-    void start(){
-        t.start();
-    }
-    
-    void stop(){
-        shouldStop = true;
-    }
-    
-    @Override
-    public void run(){
-        long beforeTime, timeDiff, sleep;
+	private final Thread t;
+	private final Board b;
+	private Set<Character> characters;
+	private final Drone d;
+	private volatile boolean shouldStop = false;
+	public static int collisions;
 
-        beforeTime = System.currentTimeMillis();
-        int animationDelay = 5;
-        long time = System.currentTimeMillis();
-        
-        while(!shouldStop){
-            characters = b.getCharacters();
-            Character c;
-            
-            synchronized(characters){
-                Iterator<Character> it = characters.iterator();
-                
-                while (it.hasNext()){
-                    c = it.next();
-                    
-                    if (!c.isDrone() && c.isColliding(d)){
-                        it.remove();
-                        // Add scoreboard stuff here
-                    }
-                }
-            }
-            try {
-                time += animationDelay;
-                Thread.sleep(Math.max(0, time
-                        - System.currentTimeMillis()));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	CollisionMonitorThread(Board b) {
+		t = new Thread(this, "Collision Monitor Thread");
+		this.b = b;
+		d = b.getDrone();
+		collisions = 0;
+	}
+
+	void start() {
+		t.start();
+	}
+
+	void stop() {
+		shouldStop = true;
+	}
+
+	@Override
+	public void run() {
+
+		int animationDelay = 5;
+		long time = System.currentTimeMillis();
+
+		while (!shouldStop) {
+			characters = b.getCharacters();
+			Character c;
+
+			synchronized (characters) {
+				Iterator<Character> it = characters.iterator();
+
+				while (it.hasNext()) {
+					c = it.next();
+
+					if (!c.isDrone() && c.isColliding(d)) {
+						it.remove();
+						collisions++;
+					}
+				}
+			}
+			try {
+				time += animationDelay;
+				Thread.sleep(Math.max(0, time - System.currentTimeMillis()));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
